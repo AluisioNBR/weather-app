06a989db3078e5b9cfb67af27d4107d9f7812233
AluisioNBR <lulu70638@gmail.com>
2022-03-09 18:46:55 -0300

    additional data add

--- App.tsx
+++ App.tsx
@@ -1,8 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, /*PermissionsAndroid*/ } from 'react-native';
 import { useState } from 'react'
 
-function CitySelection({ setTemperatureVisibility, setCityName, setCountry, setTemperatureValue, setWeatherDescription }){
+import { styles } from './styles.tsx'
+
+/*
+const requestCameraPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_BACKGROUND_LOCATION,
+      {
+        title: "Permissão de localização",
+        message:
+          "Weather Report precisa acessar sua localização para um atendimento imediato.",
+        buttonNeutral: "Pergunte depois",
+        buttonNegative: "Não autorizar",
+        buttonPositive: "Autorizar"
+      }
+    )
+
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log("Permissão de localização conced
+      a!");
+    }
+
+    else {
+      console.log("Permissão de localização negada!");
+    }
+  }
+
+  catch (err) {
+    console.warn(err);
+  }
+}
+*/
+
+function CitySelection({
+  setMsgValue,
+  setTemperatureVisibility,
+  setCityName,
+  setCountry,
+  setTemperatureValue,
+  setWeatherDescription,
+  setWeatherIcon,
+  setfeels_likeValue,
+  setTemperatureMax,
+  setTemperatureMin,
+  setHumidityValue
+}){
   const [cityValue, setCityValue] = useState('')
 
   async function WeatherInf(){
@@ -13,20 +58,30 @@ function CitySelection({ setTemperatureVisibility, setCityName, setCountry, setT
   }
   
   async function submitCity(){
-    if(cityValue.trim() == '') setTemperatureVisibility(false)
-
-    else {
       const inf = await WeatherInf()
 
-      setCityName(inf.city)
-      setCountry(inf.country)
-      setTemperatureValue(inf.temperature)
-      setWeatherDescription(inf.description)
-      setTemperatureVisibility(true)
+      if(inf.cod == 200){
+        setCityName(inf.city)
+        setCountry(inf.country)
+        setWeatherIcon(inf.icon)
+        setTemperatureValue(inf.main.temperature)
+        setWeatherDescription(inf.description)
+
+        console.log(inf.main.feels_like)
+        setfeels_likeValue(inf.main.feels_like)
+        setTemperatureMax(inf.main.temp_max)
+        setTemperatureMin(inf.main.temp_min)
+        setHumidityValue(inf.main.humidity)
+
+        setTemperatureVisibility(true)
+      }
+
+      else {
+        setMsgValue(inf.msg)
+        setTemperatureVisibility(false)
+      }
 
       return ''
-      console.log(cityValue)
-    }
   }
 
   return(
@@ -39,7 +94,7 @@ function CitySelection({ setTemperatureVisibility, setCityName, setCountry, setT
           </View>
           <TextInput required defaultValue={cityValue} onChangeText={(newText) => setCityValue(newText)} style={[styles.formCityButtonInput, { backgroundColor: '#fdfdfd', borderWidth: 2 }]}/>
 
-          <Button onPress={() => setCityValue(submitCity())} style={[styles.formCityButtonInput]} title='Selecionar
+          <Button onPress={() => setCityValue(submitCity())} color='gray' title='Selecionar
           ' />
         </View>
       </View>
@@ -47,27 +102,104 @@ function CitySelection({ setTemperatureVisibility, setCityName, setCountry, setT
 }
 
 function MainTemperature(props){
-  if(props.visibility) return (
+  return (
     <View style={[styles.MainTemperature, { padding: 16 }]}>
-      <Text>
-        <Text style={styles.local}>
-          {props.city}, {props.country}
-        </Text>
-      </Text>
-
-      <View>
-        <View>
-          <Text style={styles.currentTemperatureWeatherDescription}>
-            {props.temperature}°C
+        <Text>
+          <Text style={styles.local}>
+            {props.city}, {props.country}
           </Text>
-        </View>
+        </Text>
 
         <View>
-          <Text style={styles.currentTemperatureWeatherDescription}>
-            {props.description}
+          <View>
+            <Text style={[styles.currentTemperatureWeatherDescription, styles.currentTemperature]}>
+              {props.temperature}°C
+            </Text>
+          </View>
+
+          <View>
+            <Text style={styles.currentTemperatureWeatherDescription}>
+              {props.description}
+            </Text>
+          </View>
+        </View>
+      </View>
+  )
+}
+
+function TemperatureDetails(props){
+  return(
+    <View style={styles.temperatureDetails}>
+      <View style={styles.containerDetails}>
+        <Text style={{ textAlign: center }}>
+          Sensação Térmica
+        </Text>
+
+        <View style={styles.categoryValue}>
+          <Text>
+            {props.feels_like}°C
           </Text>
         </View>
       </View>
+
+      <View style={[styles.containerDetails, styles.minMax]}>
+        <View style={styles.minMaxDiv}>
+          <Text style={{ textAlign: center }}>
+            Max:
+          </Text>
+
+          <View style={styles.categoryValue}>
+            <Text>
+              {props.temp_max}°C
+            </Text>
+          </View>
+        </View>
+        
+        <View style={styles.minMaxDiv}>
+          <Text style={{ textAlign: center }}>
+            Min:
+          </Text>
+
+          <View style={styles.categoryValue}>
+            <Text>
+              {props.temp_min}°C
+            </Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.containerDetails}>
+        <Text style={{ textAlign: center }}>
+          Humidade
+        </Text>
+
+        <View style={styles.categoryValue}>
+          <Text>
+            {props.humidity}%
+          </Text>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+function Temperature(props){
+  if(props.visibility) return (
+    <View>
+      <MainTemperature
+        city={props.city}
+        country={props.country}
+        icon={props.icon}
+        temperature={props.temperature}
+        description={props.description}
+      />
+
+      <TemperatureDetails
+        feels_like={props.feels_like}
+        temp_max={props.temp_max}
+        temp_min={props.temp_min}
+        humidity={props.humidity}
+      />
     </View>
     )
 
@@ -82,71 +214,53 @@ function MainTemperature(props){
 
 export default function App() {
   const [temperatureVisibility, setTemperatureVisibility] = useState(false)
+
   const [cityName, setCityName] = useState('undefined')
   const [country, setCountry] = useState('undefined')
+  const [weatherIcon, setWeatherIcon] = useState('undefined.png')
   const [temperatureValue, setTemperatureValue] = useState(30)
   const [weatherDescription, setWeatherDescription] = useState('undefined')
+  
+  const [feels_likeValue, setfeels_likeValue] = useState(30)
+  const [temperatureMax, setTemperatureMax] = useState(30)
+  const [temperatureMin, setTemperatureMin] = useState(30)
+  const [humidityValue, setHumidityValue] = useState(0)
+
+  const [msgValue, setMsgValue] = useState('Informe sua cidade para começarmos!')
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
 
       <View style={styles.main}>
-        <CitySelection setTemperatureVisibility={setTemperatureVisibility} setCityName={setCityName} setCountry={setCountry} setTemperatureValue={setTemperatureValue} setWeatherDescription={setWeatherDescription} />
-        <MainTemperature city={cityName} country={country} temperature={temperatureValue} description={weatherDescription} visibility={temperatureVisibility} />
+        <CitySelection
+          setMsgValue={setMsgValue}
+          setTemperatureVisibility={setTemperatureVisibility}
+          setCityName={setCityName}
+          setCountry={setCountry}
+          setTemperatureValue={setTemperatureValue}
+          setWeatherDescription={setWeatherDescription}
+          setWeatherIcon={setWeatherIcon}
+          setfeels_likeValue={setfeels_likeValue}
+          setTemperatureMax={setTemperatureMax}
+          setTemperatureMin={setTemperatureMin}
+          setHumidityValue={setHumidityValue}
+        />
+        
+        <Temperature
+          msg={msgValue}
+          city={cityName}
+          country={country}
+          icon={weatherIcon}
+          temperature={temperatureValue}
+          description={weatherDescription}
+          feels_like={feels_likeValue}
+          temp_max={temperatureMax}
+          temp_min={temperatureMin}
+          humidity={humidityValue}
+          visibility={temperatureVisibility}
+        />
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 32,
-    backgroundColor: '#333'
-  },
-
-  main: {
-    flex: 1,
-    alignItems: 'center'
-  },
-
-  formCity: {
-    color: '#fdfdfd',
-    fontSize: 20
-  },
-
-  formCityButtonInput: {
-    fontSize: 20,
-    padding: 8
-  },
-
-  MainTemperature: {
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-
-    width: 240,
-    height:  240,
-    padding: 24,
-    borderRadius: 24,
-    
-    backgroundColor: '#0C42FF',
-    color: '#fdfdfd',
-    fontSize: 25,
-
-    textAlign: 'center'
-  },
-
-  local: {
-    color: '#fdfdfd',
-    fontSize: 25,
-    textAlign: 'center'
-  },
-
-  currentTemperatureWeatherDescription: {
-    color: '#fdfdfd',
-    fontSize: 25,
-    textAlign: 'center'
-  }
-});
